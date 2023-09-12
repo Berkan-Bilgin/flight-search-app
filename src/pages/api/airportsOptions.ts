294a3d0178dba4f9de0d095775c9622b753259cb
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2023-09-12 12:35:43 +0300

    Created API handler for fetching airport options

---
+++ src/pages/api/airportsOptions.ts
@@ -0,0 +1,6 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { airportOptions } from "@/common/airports";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.status(200).json(airportOptions);
+}
