e1106d5abdf63fc4fe5d19dfb2885734ac8689bb
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2023-09-12 19:57:43 +0300

    wip: formik validation

--- src/components/SearchForm/SearchForm.tsx
+++ src/components/SearchForm/SearchForm.tsx
@@ -10,10 +10,12 @@ const SearchSchema = Yup.object().shape({
   departureAirport: Yup.string().required('Required'),
   arrivalAirport: Yup.string().required('Required'),
   departureDate: Yup.date().required('Required'),
-  returnDate: Yup.date().nullable().when('oneWay', (oneWay, schema) => {
-    return oneWay ? schema : schema.required('Required');
-  }),
   oneWay: Yup.boolean(),
+  returnDate: Yup.date().when('oneWay', {
+    is: false, // oneWay false olduğunda
+    then: Yup.date().required('Required'), // returnDate required olacak
+    otherwise: Yup.date().nullable() // oneWay true olduğunda, returnDate isteğe bağlı olacak
+  }),
 });
 
 const CustomSelect = ({ options, field, form, onChange }) => {
@@ -25,9 +27,11 @@ const CustomSelect = ({ options, field, form, onChange }) => {
       name={field.name}
       value={options ? options.find(option => option.value === field.value) : ''}
       onChange={(option) => {
-        form.setFieldValue(field.name, option ? option.value : '');
-        if (onChange) {
-          onChange(option);
+        if (form && field) {
+          form.setFieldValue(field.name, option ? option.value : '');
+          if (onChange) {
+            onChange(option);
+          }
         }
       }}
     />
@@ -70,6 +74,7 @@ const SearchForm = () => {
       >
         {({ values, setFieldValue }) => (
           <Form>
+            {/* From */}
            <div className="mb-4">
             <label htmlFor="departureAirport" className="block text-sm font-medium text-gray-600">Departure Airport</label>
             <Field
@@ -83,6 +88,7 @@ const SearchForm = () => {
               />
             <ErrorMessage name="departureAirport" component="div" className="text-red-500 text-sm" />
           </div>
+          {/* To */}
           <div className="mb-4">
             <label htmlFor="arrivalAirport" className="block text-sm font-medium text-gray-600">Arrival Airport</label>
             <Field
@@ -92,11 +98,18 @@ const SearchForm = () => {
             />
             <ErrorMessage name="arrivalAirport" component="div" className="text-red-500 text-sm" />
           </div>
+          {/* Departure Date */}
             <div className="mb-4">
               <label htmlFor="departureDate" className="block text-sm font-medium text-gray-600">Departure Date</label>
               <Field name="departureDate" type="date" className="mt-1 p-2 w-full border rounded-md" />
               <ErrorMessage name="departureDate" component="div" className="text-red-500 text-sm" />
             </div>
+            {/* One Way */}
+            <div className="mb-4 flex items-center">
+              <Field type="checkbox" name="oneWay" className="mr-2" />
+              <label htmlFor="oneWay" className="text-sm font-medium text-gray-600">One Way</label>
+            </div>
+            {/* Return Date */}
             {!values.oneWay && (
               <div className="mb-4">
                 <label htmlFor="returnDate" className="block text-sm font-medium text-gray-600">Return Date</label>
@@ -104,10 +117,6 @@ const SearchForm = () => {
                 <ErrorMessage name="returnDate" component="div" className="text-red-500 text-sm" />
               </div>
             )}
-            <div className="mb-4 flex items-center">
-              <Field type="checkbox" name="oneWay" className="mr-2" />
-              <label htmlFor="oneWay" className="text-sm font-medium text-gray-600">One Way</label>
-            </div>
             <div>
               <button type="submit" className="bg-blue-500 text-white p-2 rounded-md w-full hover:bg-blue-600">Search</button>
             </div>
