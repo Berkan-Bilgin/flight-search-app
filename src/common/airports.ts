319d70e3d12297c03d20651f7e14edc3b83d6ba7
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2023-09-12 12:33:39 +0300

    added airportOptions

---
+++ src/common/airports.ts
@@ -0,0 +1,13 @@
+export const airportOptions = [
+    { value: 'JFK', label: 'JFK - John F. Kennedy International Airport' },
+    { value: 'LAX', label: 'LAX - Los Angeles International Airport' },
+    { value: 'ORD', label: 'ORD - O\'Hare International Airport' },
+    { value: 'SFO', label: 'SFO - San Francisco International Airport' },
+    { value: 'ATL', label: 'ATL - Hartsfield-Jackson Atlanta International Airport' },
+    { value: 'MIA', label: 'MIA - Miami International Airport' },
+    { value: 'DFW', label: 'DFW - Dallas/Fort Worth International Airport' },
+    { value: 'SEA', label: 'SEA - Seattle-Tacoma International Airport' },
+    { value: 'BOS', label: 'BOS - Logan International Airport' },
+    { value: 'MCO', label: 'MCO - Orlando International Airport' },
+    { value: 'LAS', label: 'LAS - McCarran International Airport' }
+  ];
