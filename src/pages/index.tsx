f14ec67145d45e92cbfaf39ed70a1c732ee183a2
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2023-09-13 16:41:17 +0300

    added FlightTable component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
 import Header from "@/components/Header/Header"
 import SearchForm from "@/components/SearchForm/SearchForm"
+import FlightTable from "@/components/FlightTable/FlightTable"
 export default function Home() {
   return (
     <>
     <Header></Header>
     <div className="pt-12"> {/* pt-16 ile Header yüksekliği kadar boşluk bırakılır */}
       <SearchForm></SearchForm>
-       
+      <FlightTable></FlightTable>
       </div>
     </>
     
