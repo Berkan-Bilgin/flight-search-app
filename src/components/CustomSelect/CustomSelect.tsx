5d21f2e2a288669b0c95413e3026e89fec5031a2
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2023-09-16 23:01:50 +0300

    added styles

--- src/components/CustomSelect/CustomSelect.tsx
+++ src/components/CustomSelect/CustomSelect.tsx
@@ -1,7 +1,43 @@
 import React from 'react';
 import Select, { components } from 'react-select';
 
-const CustomSelect = ({ options, field, form, onChange }) => {
+const customStyles = {
+  control: (base, state) => ({
+    ...base,
+    background: 'linear-gradient(to bottom right, #2482cf, #ffffff)',
+    
+    // match with the menu
+    borderRadius: state.isFocused ? "3px 3px 0 0" : 3,
+    // Overwrittes the different states of border
+    borderColor: state.isFocused ? "yellow" : "green",
+    // Removes weird border around container
+    boxShadow: state.isFocused ? null : null,
+    "&:hover": {
+      // Overwrittes the different states of border
+      borderColor: state.isFocused ? "red" : "blue",
+      
+
+    }
+  }),
+  menu: base => ({
+    ...base,
+    // override border radius to match the box
+    borderRadius: 0,
+    // kill the gap
+    marginTop: 0,
+    
+  }),
+  menuList: base => ({
+    ...base,
+    // kill the white space on first and last option
+    padding: 0,
+    
+  })
+};
+
+
+
+const CustomSelect = ({ options, field, form, onChange, placeholder }) => {
   const DropdownIndicator = (props) => {
     return (
       <components.DropdownIndicator {...props}>
@@ -26,6 +62,9 @@ const CustomSelect = ({ options, field, form, onChange }) => {
         }
       }}
       components={{ DropdownIndicator: null }} // This line removes the default dropdown indicator
+      placeholder={placeholder}
+      styles={customStyles}      
+      
     />
   );
 };
