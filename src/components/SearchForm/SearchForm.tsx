b6253982622f60ac8e5a8adf91abfe1436348402
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2023-09-16 11:35:26 +0300

    Improve SearchForm structure for maintainability

--- src/components/SearchForm/SearchForm.tsx
+++ src/components/SearchForm/SearchForm.tsx
@@ -1,10 +1,9 @@
-import React, {useState, useEffect} from 'react';
 import { Formik, Field, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
-import Select from 'react-select';
-import axios from 'axios';
+import CustomSelect from '../CustomSelect/CustomSelect';
 import { useFlightOptions } from '@/hooks/useFlightOptions';
-
+import TripModeSwitch from '../TripModeSwitch/TripModeSwitch';
+import useFetchAirports from '@/hooks/useFetchAirports';
 
 const SearchSchema = Yup.object().shape({
   departureAirport: Yup.string().required('Required'),
@@ -14,111 +13,101 @@ const SearchSchema = Yup.object().shape({
   returnDate: Yup.date().when('oneWay', {
     is: (oneWay: boolean) => oneWay === false,
     then: () => Yup.date().required('Required'),
-    otherwise: () => Yup.date().nullable()
+    otherwise: () => Yup.date().nullable(),
   }),
 });
-const CustomSelect = ({ options, field, form, onChange }) => {
-  return (
-    <Select
-      id="long-value-select"
-      instanceId="long-value-select"
-      options={options}
-      name={field.name}
-      value={options ? options.find(option => option.value === field.value) : ''}
-      onChange={(option) => {
-        if (form && field) {
-          form.setFieldValue(field.name, option ? option.value : '');
-          if (onChange) {
-            onChange(option);
-          }
-        }
-      }}
-    />
-  );
-};
 
-const SearchForm = () => {
-  const [airportOptions, setAirportOptions] = useState([]);
-  const {  setDepartureAirport, arrivalOptions } = useFlightOptions();
+const SearchForm = ({ onSubmit }) => {
+  const handleSubmit = (values) => {
+    if (onSubmit) {
+      onSubmit(values);
+      console.log(values);
+    }
+  };
 
-  useEffect(() => {
-    const fetchAirports = async () => {
-      try {
-        const response = await axios.get('/api/airportsOptions');
-        setAirportOptions(response.data);
-      } catch (error) {
-        console.error('Hata:', error);
-      }
-    };
-
-    fetchAirports();
-  }, []);
-
+  const { airportOptions, error } = useFetchAirports();
+  const { setDepartureAirport, arrivalOptions } = useFlightOptions();
 
   return (
-    <div className="max-w-md mx-auto bg-white p-8 rounded-lg shadow-md">
-      <h2 className="text-2xl font-semibold mb-4">Search Flights</h2>
+    <div className="max-w-md mx-auto bg-white p-8 rounded-2xl shadow-md ">
+      <h2 className="text-2xl font-semibold mb-4 ">Search Flights</h2>
       <Formik
         initialValues={{
           departureAirport: '',
           arrivalAirport: '',
           departureDate: '',
           returnDate: '',
-          oneWay: false,
+          oneWay: true,
         }}
         validationSchema={SearchSchema}
-        onSubmit={values => {
-          console.log(values);
-        }}
+        onSubmit={handleSubmit}
       >
         {({ values, setFieldValue }) => (
-          console.log("values",values),
           <Form>
-            {/* From */}
-           <div className="mb-4">
-            <label htmlFor="departureAirport" className="block text-sm font-medium text-gray-600">Departure Airport</label>
-            <Field
-              name="departureAirport"
-              component={CustomSelect}
-              options={airportOptions}
-              onChange={option => {
-                setFieldValue('departureAirport', option ? option.value : '');
-                setDepartureAirport(option ? option.value : null);
-              }}
-              />
-            <ErrorMessage name="departureAirport" component="div" className="text-red-500 text-sm" />
-          </div>
-          {/* To */}
-          <div className="mb-4">
-            <label htmlFor="arrivalAirport" className="block text-sm font-medium text-gray-600">Arrival Airport</label>
-            <Field
-              name="arrivalAirport"
-              component={CustomSelect}
-              options={arrivalOptions} // arrivalOptions'u kullanın
-            />
-            <ErrorMessage name="arrivalAirport" component="div" className="text-red-500 text-sm" />
-          </div>
-          {/* Departure Date */}
+            {/* TripModeSwitch */}
             <div className="mb-4">
-              <label htmlFor="departureDate" className="block text-sm font-medium text-gray-600">Departure Date</label>
+              <TripModeSwitch setFieldValue={setFieldValue} />
+            </div>
+            <div className="mb-4 flex justify-between ">
+              {/* From */}
+              <div className="w-1/2">
+                <label htmlFor="departureAirport" className="block text-sm font-medium text-gray-600">
+                  From
+                </label>
+                <Field
+                  name="departureAirport"
+                  component={CustomSelect}
+                  options={airportOptions}
+                  onChange={(option) => {
+                    setFieldValue('departureAirport', option ? option.value : '');
+                    setDepartureAirport(option ? option.value : null);
+                  }}
+                />
+                <ErrorMessage name="departureAirport" component="div" className="text-red-500 text-sm" />
+              </div>
+              {/* To */}
+              <div className="w-1/2">
+                <label htmlFor="arrivalAirport" className="block text-sm font-medium text-gray-600">
+                  To
+                </label>
+                <Field
+                  name="arrivalAirport"
+                  component={CustomSelect}
+                  options={arrivalOptions} // arrivalOptions'u kullanın
+                />
+                <ErrorMessage name="arrivalAirport" component="div" className="text-red-500 text-sm" />
+              </div>
+            </div>
+
+            {/* Departure Date */}
+            <div className="mb-4">
+              <label htmlFor="departureDate" className="block text-sm font-medium text-gray-600">
+                Departure Date
+              </label>
               <Field name="departureDate" type="date" className="mt-1 p-2 w-full border rounded-md" />
               <ErrorMessage name="departureDate" component="div" className="text-red-500 text-sm" />
             </div>
             {/* One Way */}
             <div className="mb-4 flex items-center">
               <Field type="checkbox" name="oneWay" className="mr-2" />
-              <label htmlFor="oneWay" className="text-sm font-medium text-gray-600">One Way</label>
+              <label htmlFor="oneWay" className="text-sm font-medium text-gray-600">
+                One Way
+              </label>
             </div>
             {/* Return Date */}
             {!values.oneWay && (
               <div className="mb-4">
-                <label htmlFor="returnDate" className="block text-sm font-medium text-gray-600">Return Date</label>
+                <label htmlFor="returnDate" className="block text-sm font-medium text-gray-600">
+                  Return Date
+                </label>
                 <Field name="returnDate" type="date" className="mt-1 p-2 w-full border rounded-md" />
                 <ErrorMessage name="returnDate" component="div" className="text-red-500 text-sm" />
               </div>
             )}
             <div>
-              <button type="submit" className="bg-blue-500 text-white p-2 rounded-md w-full hover:bg-blue-600">Search</button>
+              <button type="submit" className="bg-blue-500 text-white p-2 rounded-md w-full hover:bg-blue-600">
+                Search
+              </button>
             </div>
           </Form>
         )}
