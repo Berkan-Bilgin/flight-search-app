6260e31fb1e0c3575af2ed1b88703ff54085e872
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2023-09-16 11:37:37 +0300

    updated index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,24 @@
-import Header from "@/components/Header/Header"
-import SearchForm from "@/components/SearchForm/SearchForm"
-import FlightTable from "@/components/FlightTable/FlightTable"
+import Header from "@/components/Header/Header";
+import SearchForm from "@/components/SearchForm/SearchForm";
+import FlightTable from "@/components/FlightTable/FlightTable";
+import { useState } from "react";
+
 export default function Home() {
+  const [searchData, setSearchData] = useState(null); // [1
+
+  const handleSearchSubmit = (data: any) => {
+    setSearchData(data);
+    console.log(searchData);
+  };
   return (
     <>
-    <Header></Header>
-    <div className="pt-12"> {/* pt-16 ile Header yüksekliği kadar boşluk bırakılır */}
-      <SearchForm></SearchForm>
-      <FlightTable></FlightTable>
+      <Header></Header>
+      <div className="pt-16">
+        {" "}
+        {/* pt-16 ile Header yüksekliği kadar boşluk bırakılır */}
+        <SearchForm onSubmit={handleSearchSubmit}></SearchForm>
+        {/* <FlightTable searchData={searchData}></FlightTable> */}
       </div>
     </>
-    
-  )
+  );
 }
